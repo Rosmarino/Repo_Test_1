d9fac697048d47267e4213f3d4cd402c0ec532c9
Rosmarino <giannaccinilorenzo@gmail.com>
2018-04-07 16:19:13 +0200

    Create qsort_su_struct.c

---
+++ qsort_su_struct.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct point_struct
+{
+	int x;
+	int y;
+}coordinates;
+typedef coordinates *coordinatesList;
+
+int compare(const void *a, const void *b)
+{
+	coordinates *a1 = (coordinates *)a;
+	coordinates *b1 = (coordinates *)b;
+	if (a1->x > b1->x)
+		return 1;
+	else if (a1->x < b1->x)
+		return (-1);
+	else if (a1->x == b1->x)
+	{
+		if (a1->y < b1->y)
+			return 1;
+		else if (a1->y > b1->y)
+			return (-1);
+		else
+			return 0;
+	}
+}
+
+int main()
+{
+	int n, i;
+	scanf("%d", &n);
+	coordinatesList *A = malloc(n * sizeof(coordinates));
+	for (int i = 0; i < n; ++i)
+	{
+		A[i] = malloc(sizeof(coordinates));
+		scanf("%d%d", &A[i]->x, &A[i]->y);
+	}
+	qsort(A, n, sizeof(coordinates*), compare);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", A[i]->x);
+		printf("%d\n", A[i]->y);
+	}
+	return 0;
+}
