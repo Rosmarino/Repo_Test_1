7fa68b601fb5d6e3aa2c07b68ac1690a7c3ff3f2
Rosmarino <giannaccinilorenzo@gmail.com>
2018-05-25 15:57:53 +0200

    Update simulazione_prova_di_laboratorio.c

--- simulazione_prova_di_laboratorio.c
+++ simulazione_prova_di_laboratorio.c
@@ -4,12 +4,12 @@
 
 typedef struct patient
 {
-	char name[101];
+	char name[101]; //Max number of chars is 100
 	struct patient *next;
 }PatientOfQueue;
 typedef PatientOfQueue *QueueOfPatients;
 
-int compare(const void *a, const void *b)
+int compare(const void *a, const void *b) //Sorting function
 {
 	char **a1 = (char **)a;
 	char **b1 = (char **)b;
@@ -19,9 +19,9 @@ int compare(const void *a, const void *b)
 int main()
 {
 	int e = 1, firstOcc = 1, patientsNumber = 0, i;
-	QueueOfPatients head;
+	QueueOfPatients head; //Always points to the first patient of the queue
 	QueueOfPatients temp; //Scans the queue
-	while (e)
+	while (e) //While the event is != 0
 	{
 		scanf("%d", &e);
 		if (e == 1 && firstOcc) //First patient of queue
@@ -45,18 +45,18 @@ int main()
 		else if (e == 2) //Delete first patient of queue
 		{
 			QueueOfPatients canc = head;
-			if (head->next != NULL)
+			if (head->next != NULL) //If the queue isn't empty, scrolls to the second patient
 				head = head->next;
-			else
+			else //If the queue is empty, we're in the first occurrence again
 				firstOcc = 1;
 			free(canc);
 			patientsNumber--;
 		}
 	}
-	if (patientsNumber)
+	if (patientsNumber) //If there are patients in the queue, creates a multidimensional array of patient's names to order them alphabetically with qsort
 	{
-		char **nameArray = (char **)malloc(patientsNumber * sizeof(char *)); //Multidimensional array of patient's names
-		for (i = 0; i < patientsNumber; i++) //Creates the array of patient's names and frees the memory of the Queue
+		char **nameArray = (char **)malloc(patientsNumber * sizeof(char *)); //Multidimensional array
+		for (i = 0; i < patientsNumber; i++) //Creates the array of patient's names and frees the memory of the queue
 		{
 			nameArray[i] = (char *)malloc(101 * sizeof(char));
 			strcpy(nameArray[i], head->name);
@@ -64,10 +64,10 @@ int main()
 			head = head->next;
 			free(temp);
 		}
-		qsort(nameArray, patientsNumber, sizeof(char*), compare);
+		qsort(nameArray, patientsNumber, sizeof(char*), compare); //Sorts the patients alphabetically
 		for (i = 0; i < patientsNumber; i++)
 			printf("%s\n", nameArray[i]);
 	}
-	printf("$\n");
+	printf("$\n"); //End of the queue
 	return 0;
 }
