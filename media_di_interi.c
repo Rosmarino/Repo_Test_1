93d01b4f478b85b26498411cdf15bf05c658e938
Rosmarino <giannaccinilorenzo@gmail.com>
2018-10-18 15:45:42 +0200

    Update media_di_interi.c

--- media_di_interi.c
+++ media_di_interi.c
@@ -5,7 +5,7 @@ int main ()
 {
     int N, i = 0;
     float sum = 0;
-    while (scanf("%d",&N) != 1 || N < 0)
+    while (scanf("%d", &N) != 1 || N < 0)
     {
         printf ("Incorretto. Inserisci un intero positivo.\n");
         while(getchar() != '\n');
@@ -13,7 +13,7 @@ int main ()
     int *A = (int*)malloc(N * sizeof(int));
     while (i < N)
     {
-        while (scanf("%d",&A[i]) != 1)
+        while (scanf("%d", &A[i]) != 1)
         {
             printf ("Incorretto. Inserisci un intero.\n");
             while(getchar() != '\n');
